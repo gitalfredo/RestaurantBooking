f8bc418defa81ddbfccc8738e16048bb756db85b
Alfredo <almenaresalfredo@yahoo.com>
2018-10-23 19:56:26 -0400

    Create function to connect to database (modular effort)

--- main.py
+++ main.py
@@ -2,35 +2,41 @@ import sqlite3
 import os
 dbname = '{0}/dbsqlite.db'.format(os.path.dirname(__file__)) # Sqlite database name, including path
 
+# Connect to database, and returns the database and cursor objects
+def connect(dbname):
+    db = sqlite3.connect(dbname)  # Connect to database
+    cursor = db.cursor()
+    return db,cursor
+
+
 def displayContent(cursor):
     for row in cursor.fetchall():
         print(' '.join(str(i) for i in row))  # Display elements in row
     cursor.close()
 
 
-#All restaurants
-def listAllrestaurants():
+# All restaurants
+def listAllRestaurants():
     global dbname
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-
-        cursor = db.cursor()
+        db,cursor = connect(dbname)
         # Use all the SQL you like
         cursor.execute("SELECT * FROM Restaurant;")
 
         # Display all data
         displayContent(cursor)
+
         db.close()
+
 
     except sqlite3.Error as e:
         print ('Connection failed with error code ' + str(e.args[0]) + " " + str(e.args[1]))
 
-#All restaurants in NY
-def listAllrestaurantsNY():
+# All restaurants in NY
+def listAllRestaurantsNY():
     global dbname
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
         # Use all the SQL you like
         cursor.execute("SELECT * FROM Restaurant where city = 'NY';")
 
@@ -40,11 +46,11 @@ def listAllrestaurantsNY():
     except sqlite3.Error as e:
         print ('Connection failed with error code ' + str(e.args[0]) + " " + str(e.args[1]))
 
-#Names and addresses for customers living in NY, by customer name
-def listAllrestaurantsNYByName():
+# Names and addresses for customers living in NY, by customer name
+def listAllRestaurantsNYByName():
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
+
         # Use all the SQL you like
         cursor.execute("SELECT customerName,customerCity FROM customer WHERE customerCity LIKE '%NY%' ORDER BY customerName;")
 
@@ -54,12 +60,11 @@ def listAllrestaurantsNYByName():
     except sqlite3.Error as e:
         print ('Connection failed with error code ' + str(e.args[0]) + " " + str(e.args[1]))
 
-#Average price of Tables
+# Average price of Tables
 def averagePriceTables():
     global dbname
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
         # Use all the SQL you like
         cursor.execute("SELECT restaurantId, AVG(price) AS AvgPrice FROM Tables GROUP BY restaurantId;")
 
@@ -70,13 +75,13 @@ def averagePriceTables():
     except sqlite3.Error as e:
         print ('Connection failed with error code ' + str(e.args[0]) + " " + str(e.args[1]))
 
-#List the price and type of all Tables at the GrosveIdr restaurant
+# List the price and type of all Tables at the GrosveIdr restaurant
 def priceTypeGrosveIdr():
     global dbname
 
     try:
-        db = sqlite3.connect(dbname)             # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
+
         # Use all the SQL you like
         cursor.execute("SELECT price, type FROM Table WHERE restaurantId IN (SELECT restaurantId FROM restaurant WHERE hotenName = 'GrosveIdr');")
 
@@ -92,8 +97,8 @@ def incomePeterToday():
     global dbname
 
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
+
         # Use all the SQL you like
         cursor.execute("SELECT SUM(price) asTotalIncome FROM Tables WHERE tableId IN (SELECT tableId FROM Booking WHERE"+
                        " restaurantId IN " +
@@ -111,8 +116,8 @@ def numberTablesNY():
     global dbname
 
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
+
         # Use all the SQL you like
         cursor.execute("SELECT r.restaurantId, COUNT(tableId) AS count FROM Tables r, Restaurant h WHERE r.restaurantId = h.restaurantId AND city = 'NY' GROUP BY r.restaurantId;")
 
@@ -129,8 +134,8 @@ def commonTableNY():
     global dbname
 
     try:
-        db = sqlite3.connect(dbname)  # Connect everytime
-        cursor = db.cursor()
+        db, cursor = connect(dbname)
+
         # Use all the SQL you like
         cursor.execute("SELECT type as MostlyBooked,max(CNT)as Idoftimesbooked FROM (Select Type, count(Type) as CNT " +
                         "FROM Booking b, restaurant h, Tables r " +
@@ -147,11 +152,11 @@ def commonTableNY():
 
 
 print ( " All restaurants")
-listAllrestaurants()
+listAllRestaurants()
 print ( " All restaurants in NY")
-listAllrestaurantsNY()
+listAllRestaurantsNY()
 print ( " Names and addresses for customers living in NY, by customer name")
-listAllrestaurantsNYByName()
+listAllRestaurantsNYByName()
 print ( " Average price of Tables")
 averagePriceTables()
 print ( "------- What is the total income from bookings for the 'Peter' restaurant today?")
